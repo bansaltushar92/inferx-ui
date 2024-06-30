ed05c2e23ee284f9719b97b690d4b8952b72f2bf
Tushar Bansal <bansaltushar92@gmail.com>
2024-06-30 20:11:28 +0530

    add public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/", "/features", "/about-us"],
+    publicRoutes: ["/", "/features", "/about-us", "/dashboard"],
     // debug: true,
 });
 
